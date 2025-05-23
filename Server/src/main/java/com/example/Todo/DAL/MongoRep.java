1437267d916b1f19ad1f9deaae2a05b1d4cbc65b
Gobinath K <gobinath1019@gmail.com>
2025-05-23 06:53:13 +0530

    Delete Server directory

--- Server/src/main/java/com/example/Todo/DAL/MongoRep.java
+++
@@ -1,7 +0,0 @@
-package com.example.Todo.DAL;
-
-import com.example.Todo.Model.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MongoRep extends MongoRepository<Todo, String> {
-}
