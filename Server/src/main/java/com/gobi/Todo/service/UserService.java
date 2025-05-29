2777a61537a201389c6f22b5fc860ed7ef23bf62
Gobinath <gobinath1019@gmail.com>
2025-05-29 09:56:16 +0530

    final commit

--- Server/src/main/java/com/gobi/Todo/service/UserService.java
+++ Server/src/main/java/com/gobi/Todo/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.gobi.Todo.model.UserModel;
 import com.gobi.Todo.repository.UserRepository;
 
+import org.springframework.dao.DuplicateKeyException;
+
 import java.util.List;
 
 @Service
@@ -14,32 +16,31 @@ public class UserService {
     @Autowired
     private UserRepository userrepository;
 
-     public UserModel addUser(UserModel user) {
-        // Check if username already exists
-        if (userrepository.findByUsername(user.getUsername()).isPresent()) {
+    public UserModel addUser(UserModel user) {
+        try {
+            return userrepository.save(user);
+        } catch (DuplicateKeyException e) {
             throw new RuntimeException("Username already exists");
         }
-        return userrepository.save(user);
     }
 
-    public List<UserModel> getUser(){
-        return userrepository.findAll();    
+    public List<UserModel> getUser() {
+        return userrepository.findAll();
     }
 
-    public UserModel updateTodo(String id, UserModel user){
+    public UserModel updateTodo(String id, UserModel user) {
         UserModel existingUser = userrepository.findById(id).orElse(null);
-        if(existingUser != null){
+        if (existingUser != null) {
             existingUser.setUsername(user.getUsername());
             existingUser.setEmail(user.getEmail());
             existingUser.setPassword(user.getPassword());
             return userrepository.save(existingUser);
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public String deleteUser(String id){
+    public String deleteUser(String id) {
         userrepository.deleteById(id);
         return "User deleted successfully";
     }
